5fd93164b61ed234f6be3658e134e0e56b632edc
yanglin <yanglin5689446@gmail.com>
2018-07-24 18:58:08 +0800

    Adjust bulletins resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :bulletins
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
       resources :users, only: [:index]
+      resources :bulletins
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
