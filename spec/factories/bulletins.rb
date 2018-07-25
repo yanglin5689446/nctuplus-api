d2304d802a617e04a8bf32b492e7e821b6d8de79
yanglin <yanglin5689446@gmail.com>
2018-07-25 12:08:28 +0800

    Update bulletin factory

--- spec/factories/bulletins.rb
+++ spec/factories/bulletins.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :bulletin do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    category { Faker::Number.between(0, 1) }
+    begin_time { Faker::Time.between(30.days.ago, Date.today, :all) }
+    end_time { Faker::Time.between(Date.today, 30.days.since, :all) }
+
+    author_id { create(:user).id }
   end
 end
