231a1fe21ccc1b3f06d34cf2af25a76d9a7ae990
yanglin <yanglin5689446@gmail.com>
2018-07-27 21:15:05 +0800

    Add book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :book do
+    name { Faker::Book.title }
+    isbn { Faker::Code.isbn }
+    authors { Faker::Book.author }
+    info { Faker::Lorem.paragraph }
+    cover_image_url { Faker::Internet.url }
+    preview_url { Faker::Internet.url }
+    user { create(:user) }
+    price { Faker::Number.between(0, 3000) }
+    status { Faker::Number.between(0, 3) }
+    view_times { 0 }
   end
 end
