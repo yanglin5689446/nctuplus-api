9bc8dd3536499c699c554b7de73449ad3d852575
yanglin <yanglin5689446@gmail.com>
2018-07-27 21:16:35 +0800

    Change books routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
       resources :users, only: [:index]
       resources :bulletins
+      resources :books
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
