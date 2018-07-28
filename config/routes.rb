fbc6c5e8d2d81674cb6264da9d137a74b25dc372
yanglin <yanglin5689446@gmail.com>
2018-07-28 14:53:26 +0800

    Update event resoureces routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
       resources :users, only: [:index]
       resources :bulletins
       resources :books
+      resources :events
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
