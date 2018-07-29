e61e894bb63979c99c119d7f5691a00d6c33201d
yanglin <yanglin5689446@gmail.com>
2018-07-29 23:14:04 +0800

    Generate department model

---
+++ db/migrate/20180729151302_create_departments.rb
@@ -0,0 +1,7 @@
+class CreateDepartments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :departments do |t|
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/departments.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :department do
+  end
+end
