66c3271d86bf2e6cdb3212d767e42d81973ef2ed
yanglin <yanglin5689446@gmail.com>
2018-08-01 15:21:00 +0800

    Update permanent_course controller, only index/show

--- app/controllers/permanent_courses_controller.rb
+++ app/controllers/permanent_courses_controller.rb
@@ -1,6 +1,4 @@
 class PermanentCoursesController < ApplicationController
-  before_action :set_permanent_course, only: [:show, :update, :destroy]
-
   # GET /permanent_courses
   def index
     @permanent_courses = PermanentCourse.all
@@ -10,43 +8,7 @@ class PermanentCoursesController < ApplicationController
 
   # GET /permanent_courses/1
   def show
-    render json: @permanent_course
-  end
-
-  # POST /permanent_courses
-  def create
-    @permanent_course = PermanentCourse.new(permanent_course_params)
-
-    if @permanent_course.save
-      render json: @permanent_course, status: :created, location: @permanent_course
-    else
-      render json: @permanent_course.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /permanent_courses/1
-  def update
-    if @permanent_course.update(permanent_course_params)
-      render json: @permanent_course
-    else
-      render json: @permanent_course.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /permanent_courses/1
-  def destroy
-    @permanent_course.destroy
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_permanent_course
     @permanent_course = PermanentCourse.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def permanent_course_params
-    params.fetch(:permanent_course, {})
+    render json: @permanent_course
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index]
       resources :bulletins
       resources :books
-      resources :permanent_courses
+      resources :permanent_courses, only: [:index, :show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
