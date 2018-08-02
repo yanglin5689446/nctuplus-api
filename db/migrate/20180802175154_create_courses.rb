6838a2a407dd414efe6ef5d433457c428e80bdaf
yanglin <yanglin5689446@gmail.com>
2018-08-03 02:02:20 +0800

    Update course migration

--- db/migrate/20180802175154_create_courses.rb
+++ db/migrate/20180802175154_create_courses.rb
@@ -1,6 +1,21 @@
 class CreateCourses < ActiveRecord::Migration[5.0]
   def change
     create_table :courses do |t|
+      t.string :code
+      t.string :remarks
+      t.integer :credit
+      t.integer :requirement_type
+      t.integer :grade
+      t.string :classroom
+      t.binary :time_slots, limit: 16
+      t.integer :registration_count
+      t.integer :registration_limit
+      t.integer :assignment_record
+      t.integer :exam_record
+      t.integer :rollcall_frequency
+      t.integer :view_count
+      t.references :last_edit_user, references: :users, index: true
+      t.belongs_to :permanent_course, index: true
       t.timestamps
     end
   end
