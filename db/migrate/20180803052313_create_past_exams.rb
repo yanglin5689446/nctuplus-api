1a25e4a1306ce2e9d13e360e76105c5ef5bd9b32
yanglin <yanglin5689446@gmail.com>
2018-08-03 13:27:33 +0800

    Update past_exam migration

--- db/migrate/20180803052313_create_past_exams.rb
+++ db/migrate/20180803052313_create_past_exams.rb
@@ -1,6 +1,11 @@
 class CreatePastExams < ActiveRecord::Migration[5.0]
   def change
     create_table :past_exams do |t|
+      t.string :description
+      t.integer :download_count, default: 0
+      t.string :file
+      t.references :uploader, references: :users, index: true
+      t.belongs_to :course, index: true
       t.timestamps
     end
   end
