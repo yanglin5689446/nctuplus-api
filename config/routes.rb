c76babe4286542dbaac623dc667be5a33e009b1c
yanglin <yanglin5689446@gmail.com>
2018-08-03 14:57:22 +0800

    Remove redundant spec, update semester controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :semesters
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
-      get "colleges", to: 'colleges#index', as: :colleges_path
-      get "departments", to: 'departments#index', as: :departments_path
-      get "teachers", to: 'teachers#index', as: :teachers_path
+      get 'colleges', to: 'colleges#index', as: :colleges_path
+      get 'departments', to: 'departments#index', as: :departments_path
+      get 'teachers', to: 'teachers#index', as: :teachers_path
+      get 'semesters', to: 'semesters#index', as: :semesters_path
       resources :users, only: [:index]
       resources :bulletins
       resources :books
