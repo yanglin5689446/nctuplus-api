615dc085e54ce6821eaf299a549a442f18959186
Yang Lin <yanglin5689446@gmail.com>
2018-08-03 16:33:33 +0800

    Update timetable migration

--- db/migrate/20180803083131_create_timetables.rb
+++ db/migrate/20180803083131_create_timetables.rb
@@ -1,7 +1,8 @@
 class CreateTimetables < ActiveRecord::Migration[5.0]
   def change
     create_table :timetables do |t|
-
+      t.belongs_to :user, index: true
+      t.boolean :shareable, default: false
       t.timestamps
     end
   end
