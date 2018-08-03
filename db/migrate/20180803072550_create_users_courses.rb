fe3269cde97078d35db8294077392ee41baaedd0
Yang Lin <yanglin5689446@gmail.com>
2018-08-03 15:32:16 +0800

    Update users_course migration

--- db/migrate/20180803072550_create_users_courses.rb
+++ db/migrate/20180803072550_create_users_courses.rb
@@ -1,7 +1,8 @@
 class CreateUsersCourses < ActiveRecord::Migration[5.0]
   def change
     create_table :users_courses do |t|
-
+      t.belongs_to :user, index: true
+      t.belongs_to :course, index: true
       t.timestamps
     end
   end
