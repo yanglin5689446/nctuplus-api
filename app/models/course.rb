ad7d45fa5b637de07d00f6ed8104911dc33028fc
yanglin <yanglin5689446@gmail.com>
2018-08-03 12:26:53 +0800

    Update course relationship and factory

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+  belongs_to :last_edit_user, class_name: :User, optional: true
+  belongs_to :permanent_course
 end
--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,4 +1,23 @@
 FactoryBot.define do
   factory :course do
+    code { Faker::Number.between(1000000, 9999999) }
+    remarks { Faker::Lorem.paragraph }
+    credit { Faker::Number.between(0, 15) }
+    requirement_type { Faker::Number.between(0, 10) }
+    grade { Faker::Number.between(0, 4) }
+    classroom { Faker::Number.between(100, 999).to_s }
+    time_slots do
+      byte_array = 16.times.map { 0 }
+      rand(5).times { byte_array[rand(8)] |= 1 << rand(8) }
+      byte_array.map(&:chr).reduce(:+)
+    end
+    registration_count { 0 }
+    registration_limit { 0 }
+    assignment_record  { rand(5).times.map { 1 << rand(18) }.reduce(:+) }
+    exam_record { rand(5).times.map { 1 << rand(18) }.reduce(:+) }
+    rollcall_frequency { Faker::Number.between(0, 4) }
+    last_edit_user { create(:user) }
+    permanent_course { create(:permanent_course) }
+    view_count { 0 }
   end
 end
