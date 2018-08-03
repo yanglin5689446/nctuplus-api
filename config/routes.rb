b038baaf302e36bcb7da6520b1aeb308d62a1e51
Yang Lin <yanglin5689446@gmail.com>
2018-08-03 17:32:53 +0800

    Update timetable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :timetables
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
@@ -7,7 +6,6 @@ Rails.application.routes.draw do
       get 'departments', to: 'departments#index', as: :departments_path
       get 'teachers', to: 'teachers#index', as: :teachers_path
       get 'semesters', to: 'semesters#index', as: :semesters_path
-      resources :users, only: [:index]
       
       resources :bulletins
       resources :books
@@ -17,6 +15,11 @@ Rails.application.routes.draw do
       resources :permanent_courses, only: [:index, :show]
       resources :courses, only: [:index, :show, :update]
       post 'courses/:id/rating', to: 'courses#rating', as: :course_rating_path
+
+      resources :users, only: [:index]
+      scope 'users/(:id)' do
+        resources :timetables
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
