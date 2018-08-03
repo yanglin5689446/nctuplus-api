53482825220f088cee0fb5452c9bc75049a7ea52
yanglin <yanglin5689446@gmail.com>
2018-08-03 13:54:45 +0800

    Update past_exam model and factory

--- app/models/past_exam.rb
+++ app/models/past_exam.rb
@@ -1,2 +1,5 @@
 class PastExam < ApplicationRecord
+  belongs_to :course
+  belongs_to :uploader, class_name: :User
+  mount_base64_uploader :file, PastExamUploader
 end
--- spec/factories/past_exams.rb
+++ spec/factories/past_exams.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :past_exam do
+    description { Faker::Lorem.sentence }
+    download_count { 0 }
+    file do
+      Rack::Test::UploadedFile.new(
+        Rails.root.join('spec', 'support', 'past_exams', 'past_exam.jpg'),
+        'image/jpeg'
+      )
+    end
+    uploader { create(:user) }
+    course { create(:course) }
   end
 end
