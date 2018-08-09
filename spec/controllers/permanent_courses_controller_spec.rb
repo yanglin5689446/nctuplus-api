c87cd03618fdfbfa74825e6232126c99199db4fe
Yang Lin <yanglin5689446@gmail.com>
2018-08-09 14:51:38 +0800

    Update permanent_course spec

--- spec/controllers/permanent_courses_controller_spec.rb
+++ spec/controllers/permanent_courses_controller_spec.rb
@@ -28,11 +28,7 @@ RSpec.describe PermanentCoursesController, type: :controller do
   # PermanentCourse. As you add validations to PermanentCourse, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) do
-    skip('Add a hash of attributes valid for your model')
-  end
-
-  let(:invalid_attributes) do
-    skip('Add a hash of attributes invalid for your model')
+    FactoryBot.attributes_for :permanent_course
   end
 
   # This should return the minimal set of values that should be in the session
