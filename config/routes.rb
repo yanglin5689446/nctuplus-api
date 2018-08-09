9dafc2d60fb9574828c8c855575718094c8e3583
Yang Lin <yanglin5689446@gmail.com>
2018-08-10 01:26:41 +0800

    Remove _path postfix

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
-      get 'colleges', to: 'colleges#index', as: :colleges_path
-      get 'departments', to: 'departments#index', as: :departments_path
-      get 'teachers', to: 'teachers#index', as: :teachers_path
-      get 'semesters', to: 'semesters#index', as: :semesters_path
+      get 'colleges', to: 'colleges#index', as: :colleges
+      get 'departments', to: 'departments#index', as: :departments
+      get 'teachers', to: 'teachers#index', as: :teachers
+      get 'semesters', to: 'semesters#index', as: :semesters
 
       resources :bulletins
       resources :books
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
       resources :permanent_courses, only: [:index, :show]
       resources :courses, only: [:index, :show, :update]
-      post 'courses/:id/rating', to: 'courses#rating', as: :course_rating_path
+      post 'courses/:id/rating', to: 'courses#rating', as: :course_rating
 
       resources :users, only: [:index]
       scope 'users/(:id)' do
