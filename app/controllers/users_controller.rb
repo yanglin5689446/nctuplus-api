291b9a5b97dbbffd46c15721e792152d8e2afdc8
Yang Lin <yanglin5689446@gmail.com>
2018-08-17 15:04:37 +0800

    Add filters and pagination to users#index API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: @users
+    page = params[:page].try(:to_i) || 1
+    per_page = params[:per_page].try(:to_i) || 25
+    filters = User.ransack(params[:q])
+
+    users = filters.result(distnct: true).page(page).per(per_page)
+
+    render json: {
+      current_page: page,
+      total_pages: users.total_pages,
+      total_count: users.total_count,
+      data: users
+    }
   end
 end
