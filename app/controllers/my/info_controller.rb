b9c669f7317e208db2258d6caa8da6d372493829
Yang Lin <yanglin5689446@gmail.com>
2018-08-19 05:23:38 +0800

    Update user relation, user-info api

--- app/controllers/my/info_controller.rb
+++ app/controllers/my/info_controller.rb
@@ -1,2 +1,31 @@
 class My::InfoController < ApplicationController
+  before_action :authenticate_user!
+
+  # GET /my/events
+  def events
+    events = current_user.events
+
+    render json: events
+  end
+
+  # GET /my/books
+  def books
+    books = current_user.books
+
+    render json: books
+  end
+
+  # GET /my/past_exams
+  def past_exams
+    past_exams = current_user.past_exams
+
+    render json: past_exams
+  end
+
+  # GET /my/courses
+  def courses
+    courses = current_user.courses
+
+    render json: courses
+  end
 end
--- spec/controllers/my/info_controller_spec.rb
+++ spec/controllers/my/info_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe My::InfoController, type: :controller do
+  let(:current_user) { FactoryBot.create :user }
+  before(:each) do
+    request.headers.merge! current_user.create_new_auth_token
+  end
 
+  describe 'GET #books' do
+    it 'returns a success response' do
+      get :books, params: {}
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #events' do
+    it 'returns a success response' do
+      get :events, params: {}
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #past_exams' do
+    it 'returns a success response' do
+      get :past_exams, params: {}
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #courses' do
+    it 'returns a success response' do
+      get :courses, params: {}
+      expect(response).to be_successful
+    end
+  end
 end
