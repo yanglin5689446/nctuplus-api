db60632210d8bef359ef8d58961e0a3faf85d5da
Yang Lin <yanglin5689446@gmail.com>
2018-09-04 13:24:07 +0800

    Modify routes config, add action method

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   scope 'api' do
     scope 'v1' do
-      get 'colleges', to: 'colleges#index', as: :colleges
-      get 'departments', to: 'departments#index', as: :departments
-      get 'teachers', to: 'teachers#index', as: :teachers
-      get 'semesters', to: 'semesters#index', as: :semesters
-
+      resources :colleges, only: [:index]
+      resources :departments, only: [:index]
+      resources :teachers, only: [:index]
+      resources :semesters, only: [:index]
       resources :bulletins
       resources :books
       resources :past_exams
@@ -14,18 +13,22 @@ Rails.application.routes.draw do
         post 'follow', to: 'events#follow', as: :follow
         delete 'follow', to: 'events#unfollow', as: :unfollow
       end
-
       resources :permanent_courses, only: [:index, :show]
-      resources :courses, only: [:index, :show, :update]
-      post 'courses/:course_id/rating', to: 'courses#rating', as: :course_rating
-
+      resources :courses, only: [:index, :show, :update] do
+        post 'rating', to: 'courses#rating', as: :rating
+        post 'favorite', to: 'courses#favorite', as: :favorite
+        delete 'favorite', to: 'courses#remove_favorite', as: :remove_favorite
+      end
       resources :users, only: [:index]
+
+      # 跟已登入 user 相關路由
+      # 所有此 namespace 下的路由都要過 auth
       namespace 'my' do
-        get '/books', to: 'info#books', as: :my_books
-        get '/events', to: 'info#events', as: :my_events
-        get '/courses', to: 'info#courses', as: :my_courses
-        get '/past_exams', to: 'info#past_exams', as: :my_past_exams
-        get '/graduation_progress', to: 'info#graduation_progress', as: :my_graduation_progress
+        get '/books', to: 'info#books', as: :books
+        get '/events', to: 'info#events', as: :events
+        get '/courses', to: 'info#courses', as: :courses
+        get '/past_exams', to: 'info#past_exams', as: :past_exams
+        get '/graduation_progress', to: 'info#graduation_progress', as: :graduation_progress
         resources :timetables
       end
     end
