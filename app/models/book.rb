243495aa7ad8b41b319c371e3e1101ffcd9edf42
Yang Lin <yanglin5689446@gmail.com>
2018-09-16 19:38:14 +0800

    Add book serialize hash function

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,11 @@ class Book < ApplicationRecord
   has_many :courses, through: :books_courses
 
   validates_numericality_of :price, only_integer: true
+
+  def serializable_hash(options = {})
+    super({ **options, except: :user_id }).tap do |result|
+      result[:user] = user
+      result[:courses] = courses
+    end
+  end
 end
