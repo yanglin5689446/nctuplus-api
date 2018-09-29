627b2fff69f7c6116a1a41d8425a38008264554a
Marmot Yang <s1357andy871207@gmail.com>
2018-09-29 21:37:35 +0800

    "books api, leaving cover_image incompleted."

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,22 @@ class BooksController < ApplicationController
   # GET /books
   def index
     page = params[:page].try(:to_i) || 1
-    per_page = params[:per_page].try(:to_i) || 15
-    filters = Book.includes(:courses, :user).ransack(params[:q])
+    per_page = params[:per_page].try(:to_i) || 25
+    filters = Book.ransack(params[:q])
     @books = filters.result(distnct: true).page(page).per(per_page)
+
+    response_list = []
+    @books.each do |book|
+      response_list.push(response_assignment(book, User.find(book.user_id)))
+    end
+
+    response_list = sort_index(response_list, params)
 
     render json: {
       current_page: page,
       total_pages: @books.total_pages,
       total_count: @books.total_count,
-      data: @books
+      data: response_list
     }
   end
 
@@ -20,15 +27,21 @@ class BooksController < ApplicationController
   def show
     @book.view_count += 1
     @book.save
-    render json: @book
+
+    response = response_assignment(@book, User.find(@book.user_id))
+
+    render json: response
   end
 
   # POST /books
   def create
     @book = Book.new(book_params)
 
+    @book.user_id = current_user.id
+
     if @book.save
-      render json: @book, status: :created, location: @book
+      response = response_assignment(@book, current_user)
+      render json: response
     else
       render json: @book.errors, status: :unprocessable_entity
     end
@@ -37,7 +50,10 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1
   def update
     if @book.update(book_params)
-      render json: @book
+
+      response = response_assignment(@book, current_user)
+
+      render json: response
     else
       render json: @book.errors, status: :unprocessable_entity
     end
@@ -52,7 +68,7 @@ class BooksController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_book
-    @book = Book.includes(:courses, :user).find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
@@ -60,7 +76,39 @@ class BooksController < ApplicationController
     params.fetch(:book, {})
           .permit(
             :time, :name, :authors, :info, :cover_image,
-            :preview_url, :price, :status
+            :preview_url, :price, :status, :isbn
           )
+  end
+
+  def response_assignment(book, user)
+    response = {}
+    response[:id] = book.id
+    response[:name] = book.name
+    response[:authors] = book.authors
+    response[:isbn] = book.isbn
+    response[:user] = user
+
+    response[:cover_image] = book.cover_image
+    response[:price] = book.price
+    response[:info] = book.info
+    response[:contact_way] = user.email
+    response[:status] = book.status
+    response[:view_count] = book.view_count
+    response[:created_at] = book.created_at
+    response[:updated_at] = book.updated_at
+
+    response[:courses] = book.courses
+    response
+  end
+
+  def sort_index(response_list, params)
+    if params[:sort_by] == 'price'
+      response_list.sort_by! { |a| a[:price] }
+    elsif params[:sort_by] == 'time' && params[:descend] == true
+      response_list.sort_by! { |a| a[:created_at] }.reverse
+    else
+      response_list.sort_by! { |a| a[:created_at] }
+    end
+    response_list
   end
 end
