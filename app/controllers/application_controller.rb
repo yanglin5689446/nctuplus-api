23ca5578ded40c5425a33e718de06aa4a26d13fa
Yu-Hao Chang <9203700@gmail.com>
2018-10-04 15:22:51 +0800

    Add exception handler if record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   protected
 
   # define /auth sign up API permited parameters
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :admission_year])
+  end
+
+  def render_404
+    render status: :not_found
   end
 end
